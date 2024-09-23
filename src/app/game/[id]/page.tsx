851063c5115988b02092399637c2f6c6f2e459cb
natanlimaz <natanlimadev@gmail.com>
2024-09-23 17:29:17 -0300

    feat: Criação e finalização das páginas de detalhes dos games e do filtro de busca

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,18 +1,129 @@
+import { GameProps } from "@/utils/types/game";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { Container } from "@/components/container";
+import { Label } from "./components/label";
+import { GameCard } from "@/components/GameCard";
+import { Metadata } from "next";
 
-type GameDetailsProps = {
+type PropsParams = {
     params: {
-        id: number;
+        id: string;
     }
 }
 
-export default function Game({ params }: GameDetailsProps) {
+export async function generateMetadata({ params }: PropsParams): Promise<Metadata> {
+    try {
+        const response: GameProps = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`, { next: { revalidate: 60 } } )
+        .then((res) => res.json())
+        .catch( () => {
+            return {
+                title: "DalyGames - Descubra jogos incríveis para se divertir."
+            }
+        });
 
-    const id = params.id;
+        return {
+            title: `${response.title} `,
+            description: `${response.description.slice(0, 100)}...`,
+            openGraph: {
+                title: response.title,
+                images: [response.image_url]
+            },
+            robots: {
+                index: true,
+                follow: true,
+                nocache: true,
+                googleBot: {
+                    index: true,
+                    follow: true,
+                    noimageindex: true
+                }
+            }
+        }
+
+    }
+    catch(error) {
+        return {
+            title: "DalyGames - Descubra jogos incríveis para se divertir."
+        }
+    }
+}
+
+async function getData(id: string) {
+    try {
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { next: { revalidate: 60 } } );
+
+        return res.json();
+    }
+    catch(error) {
+        throw new Error("Failed to fetch data");
+    }
+}
+
+async function getGameSorted() {
+    try {
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" });
+
+        return res.json();
+    }
+    catch(error) {
+        throw new Error("Failed to fetch data");
+    }
+}
+
+export default async function Game({
+    params: { id }
+} : {
+    params: { id: string }
+}) {
+    const data: GameProps = await getData(id);
+    const sortedGame: GameProps = await getGameSorted();
+
+    if(!data) {
+        redirect("/");
+    }
 
     return(
-        <div>
-            <h1>Página Game</h1>
-            <p>{id}</p>
-        </div>
+
+        <main className="w-full text-black">
+            <div className="bg-black sm:h-96 h-80 w-full relative">
+                <Image 
+                    className="object-cover w-full h-80 sm:h-96 opacity-75"
+                    src={data.image_url}
+                    alt={data.title}
+                    quality={100}
+                    priority={true}
+                    fill={true}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+                />
+            </div>
+            <Container>
+                <h1 className="font-bold text-xl my-4">{data.title}</h1>
+                <p>{data.description}</p>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.platforms.map((platform) => (
+                        <Label name={platform} key={platform} />
+                    ))}
+                </div>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.categories.map((category) => (
+                        <Label name={category} key={category} />
+                    ))}
+                </div>
+
+                <p className="mt-7 mb-2"><strong>Data de lançamento:</strong> {data.release}</p>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Jogo recomendado: </h2>
+                <div className="flex">
+                    <div className="flex-grow">
+                        <GameCard data={sortedGame}/>
+                    </div>
+                </div>
+            </Container>
+        </main>
     );
 }
